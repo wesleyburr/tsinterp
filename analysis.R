c550a341519f047ab13020582972d4aad1f016dc
Tulio Criscuolo <tcriscuolo@gmail.com>
2015-06-10 23:47:46 -0400

    Testing
    — Included some data to compare the results with original results.
    — Tests are OK.

--- analysis.R
+++ analysis.R
@@ -29,19 +29,30 @@ Rprof(NULL)
 
 ############# Compare results to before changing code 
 
-data("flux")
 
-z1 <- flux$SagOrig
-z1[which(flux$S == FALSE)] <- NA
 
 # Unfortunately, not fast enough to run for CRAN checks
-sagInt2 <- interpolate(z = z1, gap = which(flux$S == FALSE), maxit = 3, delT = 86400)
+checkInterpolation <- function() {
+  data("flux")
+  z1 <- flux$SagOrig
+  z1[which(flux$S == FALSE)] <- NA
+  
+  sagInt <- interpolate(z = z1, gap = which(flux$S == FALSE), maxit = 3, delT = 86400)
 
-originalIntZf <- scan(file = "originalInter/originalZf-interpolate.txt", sep = ";")
-originalIntP <- scan(file = "originalInter/originalP-interpolate.txt")
-originalIntConverged  <- scan(file = "originalInter/originalConverged-interpolate.txt", what = logical())
-originalIntDiffC  <- scan(file = "originalInter/originalDiffC-interpolate.txt")
+  originalIntZf <- read.csv("../originalInter/originalZf-interpolate.csv")[ ,1]
+  all.equal(sagInt[[1]], originalIntZf);
+}
 
+######## Bi var Test
+checkBiVar <- function() {
+  data("flux")
+  z1 <- flux$SagOrig
+  z1[which(flux$S == FALSE)] <- NA
+  z2 <- flux$PentOrig
+  
+  sagInt <- BiVarInt(z1 = z1, z2 = z2, gap1 = which(flux$S == FALSE), 
+                    gap2 = NULL, maxit = 3, delT = 86400)
 
-all.equal(sagInt[[1]], originalIntZf);
-
+    originalBivar <- read.csv("../originalBivar/originalZf-bivar.csv")[ , 1]
+    all.equal(sagInt[[1]], originalBivar)
+}
